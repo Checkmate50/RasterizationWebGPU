03db8143a6f0b56071324dd915651f645774f3e0
Checkmate <dag368@cornell.edu>
2022-02-05 20:21:32 -0500

    Created folder for new example

---
+++ src_lights2/main.rs
@@ -0,0 +1,135 @@
+#![feature(never_type)]
+#![feature(map_first_last)] 
+
+mod context;
+mod scene;
+mod light;
+mod blur;
+mod texture;
+mod mesh;
+mod camera;
+mod material;
+mod sky;
+mod animation;
+
+use winit::{
+    event_loop::{EventLoop, ControlFlow},
+    event::{Event, WindowEvent, ElementState, DeviceEvent, VirtualKeyCode, KeyboardInput},
+    window::Window,
+};
+use anyhow::Result;
+use futures::executor::block_on;
+use context::Context;
+use glam::{Vec3, Mat3};
+use std::time::{Instant, Duration};
+
+fn main() -> Result<!> {
+
+    #[cfg(debug_assertions)]
+    env_logger::init(); // enable logging for vulkan validation layers
+
+    let file_path = if let Some(file_path) = std::env::args().nth(1) {
+        file_path
+    } else {
+        eprintln!("Error: Please provide a path to a glb file that has an associated json file of the same name.");
+        std::process::exit(1);
+    };
+
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop)?;
+    let mut state = block_on(Context::new(&window, file_path))?;
+    let mut clicking = false;
+    let mut x_accel = 0.0;
+    let mut y_accel = 0.0;
+    // this time stuff is a bit clunky rn, I wonder if there's a more elegant way
+    let mut start_time = Instant::now();
+    let mut pause_time: Option<Instant> = None;
+    let mut elapsed = 0.0;
+    let mut previous_time = Instant::now();
+    let mut frame_count = 0;
+    event_loop.run(move |event, _, control_flow| {
+        
+        let current_time = Instant::now();
+
+        if current_time - previous_time >= Duration::new(1, 0) {
+            println!("Current first FPS: {}", frame_count);
+            previous_time = Instant::now();
+            frame_count = 0;
+        }
+
+        frame_count += 1;
+
+        if pause_time == None {
+            let now = Instant::now();
+            if let Some(duration) = now.checked_duration_since(start_time) {
+                elapsed = duration.as_secs_f32();
+            } else {
+                start_time = Instant::now();
+                elapsed = 0.0;
+            }
+        }
+        match event {
+            Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => *control_flow = ControlFlow::Exit,
+            Event::WindowEvent { event: WindowEvent::MouseInput { state: ElementState::Pressed, .. }, .. } => clicking = true,
+            Event::WindowEvent { event: WindowEvent::MouseInput { state: ElementState::Released, .. }, .. } => clicking = false,
+            Event::WindowEvent { event: WindowEvent::KeyboardInput { input: KeyboardInput { virtual_keycode: Some(VirtualKeyCode::Space), state: ElementState::Released, .. }, .. }, .. } => {
+                if let Some(t) = pause_time {
+                    start_time += t.elapsed();
+                    pause_time = None;
+                } else {
+                    pause_time = Some(Instant::now());
+                }
+            },
+            Event::WindowEvent { event: WindowEvent::KeyboardInput { input: KeyboardInput { virtual_keycode: Some(VirtualKeyCode::R), state: ElementState::Released, .. }, .. }, .. } => {
+                start_time = Instant::now();
+                pause_time = None;
+            },
+            Event::WindowEvent { event: WindowEvent::KeyboardInput { input: KeyboardInput { virtual_keycode: Some(VirtualKeyCode::Left), state: ElementState::Pressed, .. }, .. }, .. } => {
+                start_time += Duration::new(0, 50000000);
+                if let Some(t) = pause_time {
+                    elapsed = (start_time.elapsed().saturating_sub(t.elapsed())).as_secs_f32();
+                }
+            },
+            Event::WindowEvent { event: WindowEvent::KeyboardInput { input: KeyboardInput { virtual_keycode: Some(VirtualKeyCode::Right), state: ElementState::Pressed, .. }, .. }, .. } => {
+                start_time -= Duration::new(0, 50000000);
+                if let Some(t) = pause_time {
+                    elapsed = (start_time.elapsed().saturating_sub(t.elapsed())).as_secs_f32();
+                }
+            },
+            Event::DeviceEvent { event: DeviceEvent::MouseMotion { delta: (x, y) }, .. } if clicking => {
+                x_accel = x;
+                y_accel = y;
+            },
+
+            Event::MainEventsCleared => {
+                let mut move_mat = Mat3::from_rotation_y((-x_accel as f32).to_radians());
+                let max = Vec3::new(0.0, -y_accel as f32, 0.0);
+                let angle = f32::atan2(state.scene.camera.eye.cross(max).length(), state.scene.camera.eye.dot(max));
+                if angle < 2.8 {
+                    let a = state.scene.camera.eye + state.scene.camera.target;
+                    let axis = a.cross(Vec3::new(0.0, 1.0, 0.0)).normalize();
+                    move_mat = move_mat * Mat3::from_axis_angle(axis.normalize(), (y_accel as f32).to_radians());
+                }
+                state.scene.camera.update(&state.queue, move_mat);
+
+                x_accel *= 0.95;
+                if (-0.01..0.01).contains(&x_accel) {
+                    x_accel = 0.0;
+                }
+
+                y_accel *= 0.95;
+                if (-0.01..0.01).contains(&y_accel) {
+                    y_accel = 0.0;
+                }
+
+                if state.render(elapsed).is_err() {
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+            },
+            _ => {},
+        }
+
+    })
+}
+
---
+++ src_lights2/texture.rs
@@ -0,0 +1,151 @@
+use wgpu::*;
+use core::num::NonZeroU32;
+
+pub struct Texture {
+    pub texture: wgpu::Texture,
+    pub view: TextureView,
+    pub sampler: Sampler,
+    pub bind_group: BindGroup,
+    pub format: TextureFormat,
+}
+
+impl Texture {
+    pub fn create_window_texture(device: &Device, layout: &BindGroupLayout, format: TextureFormat, compare: Option<CompareFunction>, width: u32, height: u32) -> Self {
+        let texture = device.create_texture(&TextureDescriptor {
+            size: Extent3d {
+                width,
+                height,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: TextureDimension::D2,
+            format,
+            usage: TextureUsages::RENDER_ATTACHMENT | TextureUsages::TEXTURE_BINDING,
+            label: Some("texture"),
+        });
+
+        let view = texture.create_view(&TextureViewDescriptor::default());
+
+        let sampler = device.create_sampler(&SamplerDescriptor {
+            mag_filter: FilterMode::Linear,
+            min_filter: FilterMode::Linear,
+            mipmap_filter: FilterMode::Nearest,
+            lod_min_clamp: -100.0,
+            lod_max_clamp: 100.0,
+            compare,
+            ..Default::default()
+        });
+
+        let bind_group = device.create_bind_group(&BindGroupDescriptor {
+            layout: layout,
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: BindingResource::TextureView(&view),
+                },
+                BindGroupEntry {
+                    binding: 1,
+                    resource: BindingResource::Sampler(&sampler),
+                }
+            ],
+            label: Some(&format!("{:?}", format)),
+        });
+
+
+        Self { texture, view, sampler, bind_group, format }
+    }
+}
+
+pub struct MipTexture {
+    pub texture: wgpu::Texture,
+    pub sampler: Sampler,
+    pub bind_groups: Vec<BindGroup>,
+    pub views: Vec<TextureView>,
+    pub format: TextureFormat,
+    pub mip_level_count: u32,
+}
+
+impl MipTexture {
+    pub fn new(device: &Device, layout: &BindGroupLayout, width: u32, height: u32, mip_level_count: u32) -> Self {
+        let format = TextureFormat::Rgba16Float;
+
+        let texture = device.create_texture(&TextureDescriptor {
+            size: Extent3d {
+                width,
+                height,
+                depth_or_array_layers: 1,
+            },
+            mip_level_count,
+            format,
+            sample_count: 1,
+            dimension: TextureDimension::D2,
+            usage: TextureUsages::RENDER_ATTACHMENT | TextureUsages::TEXTURE_BINDING,
+            label: Some("mip texture"),
+        });
+
+        let sampler = device.create_sampler(&SamplerDescriptor {
+            mag_filter: FilterMode::Linear,
+            min_filter: FilterMode::Linear,
+            mipmap_filter: FilterMode::Linear,
+            lod_min_clamp: -100.0,
+            lod_max_clamp: 100.0,
+            ..Default::default()
+        });
+
+        let views: Vec<TextureView> = (0..mip_level_count).map(|i| {
+            texture.create_view(&TextureViewDescriptor {
+                label: Some("mip"),
+                format: None,
+                dimension: None,
+                aspect: TextureAspect::All,
+                base_mip_level: i,
+                mip_level_count: NonZeroU32::new(1),
+                base_array_layer: 0,
+                array_layer_count: None,
+            })
+        }).collect();
+
+
+        let bind_groups = (0..mip_level_count).map(|i| {
+            device.create_bind_group(&BindGroupDescriptor {
+                layout: layout,
+                entries: &[
+                    BindGroupEntry {
+                        binding: 0,
+                        resource: BindingResource::TextureView(&views[i as usize]),
+                    },
+                    BindGroupEntry {
+                        binding: 1,
+                        resource: BindingResource::Sampler(&sampler),
+                    },
+                ],
+                label: Some(&format!("{:?} mip", format)),
+            })
+        }).collect();
+
+
+        Self { texture, sampler, bind_groups, format, mip_level_count, views }
+    }
+
+    pub fn generate_mipmaps(&self, pipeline: &RenderPipeline, encoder: &mut CommandEncoder) {
+        for i in 1..self.mip_level_count as usize {
+            let mut rpass = encoder.begin_render_pass(&RenderPassDescriptor {
+                label: Some("mip pass"),
+                color_attachments: &[RenderPassColorAttachment {
+                    view: &self.views[i],
+                    resolve_target: None,
+                    ops: Operations {
+                        load: LoadOp::Clear(Color::WHITE),
+                        store: true,
+                    },
+                }],
+                depth_stencil_attachment: None,
+            });
+            rpass.set_pipeline(&pipeline);
+            rpass.set_bind_group(0, &self.bind_groups[i - 1], &[]);
+            rpass.draw(0..4, 0..1);
+        }
+    }
+}
+
