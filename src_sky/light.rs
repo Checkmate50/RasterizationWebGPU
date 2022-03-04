5ca1a8a0af75db044106f3e5a8ed9d38c37d3cfb
Checkmate50 <dgeisler50@gmail.com>
2022-03-04 11:16:09 -0500

    Added sky binding example

---
+++ src_sky/light.rs
@@ -0,0 +1,149 @@
+use wgpu::*;
+use wgpu::util::DeviceExt;
+use serde::{Serialize, Deserialize};
+use glam::{Vec3, Vec2, Mat4};
+use crate::texture::Texture;
+use std::path::Path;
+use anyhow::Result;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type")]
+pub enum LightJSON {
+    Point {
+        node: String,
+        position: Vec3,
+        power: Vec3,
+    },
+    Area {
+        node: String,
+        position: Vec3,
+        power: Vec3,
+        normal: Vec3,
+        up: Vec3,
+        size: Vec2,
+        #[serde(default = "Vec3::default")]
+        u: Vec3,
+        #[serde(default = "Vec3::default")]
+        v: Vec3,
+    },
+    Ambient {
+        node: String,
+        radiance: Vec3,
+        range: Option<f32>,
+    },
+}
+
+impl LightJSON {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Vec<Self>> {
+        Ok(serde_json::from_slice(&bytes)?) // kinda weird
+    }
+
+    pub fn from_file(filename: impl AsRef<Path>) -> Result<Vec<Self>> {
+        let json_str = std::fs::read_to_string(filename)?;
+        Ok(serde_json::from_str(&json_str)?)
+    }
+
+    pub fn get_node(&self) -> &str {
+        match self {
+            LightJSON::Point { node, ..} | LightJSON::Area { node, .. } | LightJSON::Ambient { node, .. } => node
+        }
+    }
+
+    pub fn apply_matrix(&mut self, mat: Mat4) {
+        match self {
+            LightJSON::Point { position, .. } => {
+                *position = mat.transform_point3(*position);
+            },
+            LightJSON::Area { position, normal, up, u, v, .. } => {
+                *position = mat.transform_point3(*position);
+                *normal = mat.transform_vector3(*normal);
+                *up = mat.transform_vector3(*up);
+
+                // compute light basis here to not do later
+                *u = up.cross(*normal).normalize();
+                *v = normal.cross(*u);
+            },
+            LightJSON::Ambient { .. } => (),
+        }
+    }
+}
+
+pub enum Light {
+    Point { bind_group: BindGroup, texture: Texture },
+    Ambient { bind_group: BindGroup },
+}
+
+impl Light {
+    pub fn new_point(position: Vec3, power: Vec3, device: &Device, layout: &BindGroupLayout, texture_layout: &BindGroupLayout) -> Self {
+        let view_mat = Mat4::look_at_rh(position, Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 1.0, 0.0));
+        let proj_mat = Mat4::perspective_rh(1.0, 1.0, 1.0, 50.0);
+
+        let slice = [
+            proj_mat.col(0),
+            proj_mat.col(1),
+            proj_mat.col(2),
+            proj_mat.col(3),
+            view_mat.col(0),
+            view_mat.col(1),
+            view_mat.col(2),
+            view_mat.col(3),
+            power.extend(1.0),
+            position.extend(1.0),
+        ];
+
+        let buffer = device.create_buffer_init(&util::BufferInitDescriptor {
+            label: Some("light buffer"),
+            contents: bytemuck::cast_slice(&slice),
+            usage: BufferUsages::UNIFORM,
+        });
+
+        let bind_group = device.create_bind_group(&BindGroupDescriptor {
+            layout: layout,
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: buffer.as_entire_binding(),
+                }
+            ],
+            label: Some("light bind group"),
+        });
+
+        let texture = Texture::create_window_texture(&device, &texture_layout, TextureFormat::Depth32Float, Some(CompareFunction::LessEqual), 1024, 1024);
+
+        Self::Point {
+            bind_group,
+            texture,
+        }
+    }
+
+    pub fn new_ambient(radiance: Vec3, range: Option<f32>, device: &Device, layout: &BindGroupLayout) -> Self {
+
+        let slice = [
+            radiance.x,
+            radiance.y,
+            radiance.z,
+            range.unwrap_or(0.0),
+        ];
+
+        let buffer = device.create_buffer_init(&util::BufferInitDescriptor {
+            label: Some("ambient light buffer"),
+            contents: bytemuck::cast_slice(&slice),
+            usage: BufferUsages::UNIFORM,
+        });
+
+        let bind_group = device.create_bind_group(&BindGroupDescriptor {
+            layout: layout,
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: buffer.as_entire_binding(),
+                }
+            ],
+            label: Some("ambient light label"),
+        });
+
+        Self::Ambient {
+            bind_group,
+        }
+    }
+}
---
+++ src_sky/sky.rs
@@ -0,0 +1,189 @@
+use wgpu::*;
+use wgpu::util::DeviceExt;
+use glam::{Vec2, Vec3, Vec4};
+use core::ops::{Mul, Add};
+use std::f32::consts::PI;
+use crevice::std140::{AsStd140, Std140};
+
+pub struct Sky {
+    pub buffer: Buffer,
+    pub layout: BindGroupLayout,
+}
+
+struct Vec5 {
+    x: f32,
+    y: f32,
+    z: f32,
+    w: f32,
+    v: f32,
+}
+
+impl Vec5 {
+    fn new(x: f32, y:f32, z: f32, w: f32, v: f32) -> Self {
+        Self { x, y, z, w, v }
+    }
+}
+
+impl Mul<f32> for Vec5 {
+    type Output = Self;
+
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+            w: self.w * rhs,
+            v: self.v * rhs,
+        }
+    }
+}
+
+impl Add for Vec5 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+            w: self.w + rhs.w,
+            v: self.v + rhs.v,
+        }
+    }
+}
+
+struct Mat5x2(Vec5, Vec5);
+
+impl Mul<Vec2> for Mat5x2 {
+    type Output = Vec5;
+
+    fn mul(self, rhs: Vec2) -> Self::Output {
+        self.0 * rhs.x + self.1 * rhs.y
+    }
+}
+
+struct Mat4x3(Vec4, Vec4, Vec4);
+
+impl Mul<Vec3> for Mat4x3 {
+    type Output = Vec4;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        self.0 * rhs.x + self.1 * rhs.y + self.2 * rhs.z
+    }
+}
+
+fn y_z(theta: f32, t: f32) -> f32 {
+    let chi = (4.0 / 9.0 - t / 120.0) * (PI - 2.0 * theta);
+    (4.0453 * t - 4.9710) * f32::tan(chi) - 0.2155 * t + 2.4192
+}
+
+fn __z(theta: f32, t: f32, m: Mat4x3) -> f32 {
+    let vt = Vec3::new(t * t, t, 1.0);
+    let vth = Vec4::new(theta * theta * theta, theta * theta, theta, 1.0);
+    (m * vt).dot(vth)
+}
+
+impl Sky {
+    pub fn new(theta_sun: f32, turbidity: f32, device: &Device) -> Sky {
+
+        let c_y = Mat5x2(
+            Vec5::new(0.1787, -0.3554, -0.0227, 0.1206, -0.0670),
+            Vec5::new(-1.4630, 0.4275, 5.3251, -2.5771, 0.3703),
+            );
+        let cx = Mat5x2(
+            Vec5::new(-0.0193, -0.0665, -0.0004, -0.0641, -0.0033),
+            Vec5::new(-0.2592, 0.0008, 0.2125, -0.8989, 0.0452),
+            );
+        let cy = Mat5x2( 
+            Vec5::new(-0.0167, -0.0950, -0.0079, -0.0441, -0.0109),
+            Vec5::new(-0.2608, 0.0092, 0.2102, -1.6537, 0.0529),
+            );
+
+        let mx = Mat4x3(
+            Vec4::new(0.0017, -0.0037,  0.0021,  0.0000),
+            Vec4::new(-0.0290,  0.0638, -0.0320,  0.0039),
+            Vec4::new(0.1169, -0.2120,  0.0605,  0.2589),
+            );
+
+        let my = Mat4x3(
+            Vec4::new(0.0028, -0.0061,  0.0032,  0.0000),
+            Vec4::new(-0.0421,  0.0897, -0.0415,  0.0052),
+            Vec4::new(0.1535, -0.2676,  0.0667,  0.2669),
+            );
+
+        let p_y = c_y * Vec2::new(turbidity, 1.0);
+        let px = cx * Vec2::new(turbidity, 1.0);
+        let py = cy * Vec2::new(turbidity, 1.0);
+
+        let y_z = y_z(theta_sun, turbidity);
+        let xz = __z(theta_sun, turbidity, mx);
+        let yz = __z(theta_sun, turbidity, my);
+
+        let a = Vec3::new(p_y.x, px.x, py.x);
+        let b = Vec3::new(p_y.y, px.y, py.y);
+        let c = Vec3::new(p_y.z, px.z, py.z);
+        let d = Vec3::new(p_y.w, px.w, py.w);
+        let e = Vec3::new(p_y.v, px.v, py.v);
+        let zenith = Vec3::new(y_z, xz, yz);
+        let bytes = SkyBytes {
+            a,
+            b,
+            c,
+            d,
+            e,
+            zenith,
+            theta_sun,
+        };
+
+        let layout = device.create_bind_group_layout(&BindGroupLayoutDescriptor {
+            entries: &[
+                BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: ShaderStages::FRAGMENT,
+                    ty: BindingType::Buffer {
+                        ty: BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
+            label: Some("sky layout"),
+        });
+
+        let buffer = device.create_buffer_init(&util::BufferInitDescriptor {
+            label: Some("sky buffer"),
+            contents: bytes.as_std140().as_bytes(),
+            usage: BufferUsages::UNIFORM,
+        });
+
+        // let bind_group = device.create_bind_group(&BindGroupDescriptor {
+        //     layout: &layout,
+        //     entries: &[
+        //         BindGroupEntry {
+        //             binding: 0,
+        //             resource: buffer.as_entire_binding(),
+        //         },
+        //     ],
+        //     label: Some("sky bind group"),
+        // });
+
+
+        Self {
+            buffer, 
+            layout,
+        }
+    }
+}
+
+#[derive(AsStd140)]
+struct SkyBytes {
+    a: Vec3,
+    b: Vec3,
+    c: Vec3,
+    d: Vec3,
+    e: Vec3,
+    zenith: Vec3,
+    theta_sun: f32,
+}
+
